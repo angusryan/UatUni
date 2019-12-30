454fd3eac99273189308ce7822e37cfad9eccceb
Angus Ryan <angusryan@outlook.com>
2019-12-30 14:52:51 +1100

    Added comments to code

--- Activity C (Raspberry Pi)/Example/DHT11Example.py
+++ Activity C (Raspberry Pi)/Example/DHT11Example.py
@@ -4,12 +4,15 @@ import Adafruit_DHT
 
 while True:
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    humidityString = str(humidity)
-    temperatureString = str(temperature)
-    dataString = temperatureString + " " + humidityString
-    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
-    fb = open('/home/pi/data', 'a+')
-    fb.write(dataString)
-    fb.write('\n')
-    fb.close()
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4) #Calls the Library which fetches the Humidity and Temperature values and assigns them to variables.
+    
+    humidityString = str(humidity) #convert Humidity float to string
+    temperatureString = str(temperature) #convert Temperature float to string
+    dataString = temperatureString + " " + humidityString #Concatenate two strings into one
+    
+    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)) #Print values in terminal
+    
+    fb = open('/home/pi/data', 'a+') #Open text file based on assigned directory
+    fb.write(dataString) #Write your values temperature and humidity to the text file
+    fb.write('\n') #Go to a new line ready for the next value input
+    fb.close() #Close to save the new updates to the file
