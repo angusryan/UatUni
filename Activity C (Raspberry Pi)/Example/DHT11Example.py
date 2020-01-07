e5c71aeb8aeb45594595deea2913c575fc9e2e7a
Angus Ryan <angusryan@outlook.com>
2020-01-07 15:08:30 +1100

    Revised Code to ensure DHT11 works in future

--- Activity C (Raspberry Pi)/Example/DHT11Example.py
+++ Activity C (Raspberry Pi)/Example/DHT11Example.py
@@ -1,18 +1,35 @@
-#!/usr/bin/python
-import sys
 import Adafruit_DHT
+import time
+import sys
 
-while True:
+sensor=Adafruit_DHT.DHT11
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4) #Calls the Library which fetches the Humidity and Temperature values and assigns them to variables.
+gpio=17
+
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor,gpio)
+
+try:
+    while True:
+        time.sleep(0.5)
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+            
+            
+            humidityString = str(humidity) #convert Humidity float to string
+            temperatureString = str(temperature) #convert Temperature float to string
+            dataString = temperatureString + " " + humidityString #Concatenate two strings into one
     
-    humidityString = str(humidity) #convert Humidity float to string
-    temperatureString = str(temperature) #convert Temperature float to string
-    dataString = temperatureString + " " + humidityString #Concatenate two strings into one
     
-    print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)) #Print values in terminal
     
-    fb = open('/home/pi/data', 'a+') #Open text file based on assigned directory
-    fb.write(dataString) #Write your values temperature and humidity to the text file
-    fb.write('\n') #Go to a new line ready for the next value input
-    fb.close() #Close to save the new updates to the file
+            fb = open('/home/pi/data', 'a+') #Open text file based on assigned directory
+            fb.write(dataString) #Write your values temperature and humidity to the text file
+            fb.write('\n') #Go to a new line ready for the next value input
+            fb.close() #Close to save the new updates to the file
+
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
+    
+except KeyboardInterrupt: #Break with CTRL+C
+    print ("Cleaning up!")
